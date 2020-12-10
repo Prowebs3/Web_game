10066cf550d456c966e2d0c4cf997b97d71b7255
Rahul <rahulrajeev067@gmail.com>
2020-12-10 11:02:24 +0530

    index file added

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+ include "server.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <embed src="kids.mp3" height="0" width="0" autostart="true">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <center>
+        <div class="cen">
+            <button class="btn fourth" onclick="location.href='login.php'">PLAY GAME </button>
+            <button class="btn fourth">HELP?</button>
+
+        </div>
+
+    </center>
+
+
+
+</body>
+
+</html>
